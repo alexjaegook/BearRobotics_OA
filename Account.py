eebeecaba43c3b90ae86362f18da8376f0dbbafd
Alex Kim <alexander.jg.kim@gmail.com>
2025-01-25 15:10:56 -0500

    adding type hints, and adding Banking System

--- Account.py
+++ Account.py
@@ -1,23 +1,24 @@
 class Account:
-    def __init__(self, account_number, pin, balance=0):
+    def __init__(self, account_number: str, account_type: str, pin: str, balance: int=0) -> None:
         self.account_number = account_number
-        self.balance = balance
+        self.account_type = account_type
         self.pin = pin
+        self.balance = balance
 
-    def verify_pin(self, pin):
+    def verify_pin(self, pin: str) -> bool:
         return self.pin == pin
 
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> None:
         if amount <= 0:
             raise ValueError("Deposit amount must be greater than zero.")
         self.balance += amount
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: int) -> None:
         if amount > self.balance:
             raise ValueError("Insufficient funds.")
         if amount <= 0:
             raise ValueError("Withdrawal amount must be greater than zero.")
         self.balance -= amount
 
-    def get_balance(self):
+    def get_balance(self) -> int:
         return self.balance
